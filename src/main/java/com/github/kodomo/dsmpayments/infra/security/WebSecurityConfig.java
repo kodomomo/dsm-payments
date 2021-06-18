7c30e898023106d5b6d884c7485622bede3c6d3f
minki son <migsking@naver.com>
2021-06-18 20:23:17 +0900

    [ADD] Interceptor

--- src/main/java/com/github/kodomo/dsmpayments/infra/security/WebSecurityConfig.java
+++ src/main/java/com/github/kodomo/dsmpayments/infra/security/WebSecurityConfig.java
@@ -1,15 +1,26 @@
 package com.github.kodomo.dsmpayments.infra.security;
 
+import com.github.kodomo.dsmpayments.infra.token.TokenHandler;
+import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
+@RequiredArgsConstructor
 @EnableWebSecurity
 @Configuration
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter implements WebMvcConfigurer {
+
+    private final TokenHandler tokenHandler;
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(tokenHandler);
+    }
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
