1a7d75724b694fc947450a50d1420b2499d326ac
minki son <migsking@naver.com>
2021-06-24 16:38:57 +0900

    [ADD] 메서드

--- src/main/java/com/github/kodomo/dsmpayments/domain/booth/controller/BoothController.java
+++ src/main/java/com/github/kodomo/dsmpayments/domain/booth/controller/BoothController.java
@@ -2,9 +2,13 @@ package com.github.kodomo.dsmpayments.domain.booth.controller;
 
 import com.github.kodomo.dsmpayments.domain.booth.controller.payload.request.BoothLoginRequest;
 import com.github.kodomo.dsmpayments.domain.booth.controller.payload.request.CreateMenuRequest;
+import com.github.kodomo.dsmpayments.domain.booth.controller.payload.request.PaymentPermissionRequest;
+import com.github.kodomo.dsmpayments.domain.booth.controller.payload.request.PaymentRequest;
 import com.github.kodomo.dsmpayments.domain.booth.controller.payload.response.BoothLoginResponse;
 import com.github.kodomo.dsmpayments.domain.booth.controller.payload.response.GetMenusResponse;
+import com.github.kodomo.dsmpayments.domain.booth.controller.payload.response.PaymentResponse;
 import com.github.kodomo.dsmpayments.domain.booth.service.BoothService;
+import com.github.kodomo.dsmpayments.domain.receipt.service.dto.ReceiptDTO;
 import com.github.kodomo.dsmpayments.infra.token.JWTRequired;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.repository.query.Param;
@@ -30,20 +34,42 @@ public class BoothController {
     @JWTRequired
     @ResponseStatus(HttpStatus.CREATED)
     public void createMenu(@AuthenticationPrincipal Object boothId, @RequestBody CreateMenuRequest request) {
+        System.out.println(boothId);
         boothService.createMenu((String) boothId, request.getName(), request.getPrice());
     }
 
     @GetMapping("/menu")
     @JWTRequired
-    @ResponseStatus(HttpStatus.CREATED)
+    @ResponseStatus(HttpStatus.OK)
     public GetMenusResponse getMenus(@AuthenticationPrincipal Object boothId) {
         return GetMenusResponse.of(boothService.getMenus((String) boothId));
     }
 
     @DeleteMapping("/menu")
     @JWTRequired
-    @ResponseStatus(HttpStatus.CREATED)
-    public void deleteMenus(@AuthenticationPrincipal Object boothId, @RequestParam("menu-id") String menuId) {
+    @ResponseStatus(HttpStatus.OK)
+    public void deleteMenus(@AuthenticationPrincipal Object boothId, @RequestParam("id") String menuId) {
         boothService.deleteMenu((String) boothId, Integer.parseInt(menuId));
+    }
+
+    @PostMapping("/menu/payment")
+    @JWTRequired
+    @ResponseStatus(HttpStatus.OK)
+    public PaymentResponse payment(@AuthenticationPrincipal Object boothId, @RequestBody PaymentRequest request) {
+        ReceiptDTO receipt = boothService.pay((String) boothId, request.getMenuId(), request.getUserUuid());
+        return PaymentResponse.builder()
+                .boothId(receipt.getBooth().getBoothId())
+                .userUuid(receipt.getUser().getUserUuid())
+                .requestedCoin(receipt.getRequestValue())
+                .tax(receipt.getTax())
+                .finalCoin(receipt.getFinalValue())
+                .build();
+    }
+
+    @PostMapping("/menu/payment/permission")
+    @JWTRequired
+    @ResponseStatus(HttpStatus.ACCEPTED)
+    public void permitPayment(@AuthenticationPrincipal Object boothId, @RequestBody PaymentPermissionRequest request) {
+        boothService.permitPayment((String) boothId, request.getUserUuid());
     }
 }
