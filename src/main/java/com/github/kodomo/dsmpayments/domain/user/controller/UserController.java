4600b86abd7ba297b35c385e8fccf5fafd8eb283
minki son <migsking@naver.com>
2021-06-24 16:41:20 +0900

    [MODIFY] 디버그 로직 삭제

--- src/main/java/com/github/kodomo/dsmpayments/domain/user/controller/UserController.java
+++ src/main/java/com/github/kodomo/dsmpayments/domain/user/controller/UserController.java
@@ -1,12 +1,14 @@
 package com.github.kodomo.dsmpayments.domain.user.controller;
 
 import com.github.kodomo.dsmpayments.domain.user.controller.payload.request.UserLoginRequest;
+import com.github.kodomo.dsmpayments.domain.user.controller.payload.response.GetUserResponse;
 import com.github.kodomo.dsmpayments.domain.user.controller.payload.response.UserLoginResponse;
 import com.github.kodomo.dsmpayments.domain.user.controller.payload.response.UserResponse;
 import com.github.kodomo.dsmpayments.domain.user.entity.User;
 import com.github.kodomo.dsmpayments.domain.user.service.UserService;
 import com.github.kodomo.dsmpayments.infra.token.JWTRequired;
 import com.github.kodomo.dsmpayments.infra.token.TokenHandler;
+import com.github.kodomo.dsmpayments.infra.token.TokenProvider;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
@@ -20,6 +22,8 @@ import javax.lang.model.type.IntersectionType;
 public class UserController {
     private final UserService userService;
 
+    private final TokenProvider tokenProvider;
+
     @PostMapping("/auth")
     @ResponseStatus(HttpStatus.OK)
     public UserLoginResponse login(@RequestBody UserLoginRequest request) {
@@ -32,14 +36,25 @@ public class UserController {
     @GetMapping
     @ResponseStatus(HttpStatus.OK)
     public UserResponse getUser(@AuthenticationPrincipal Object number) {
-        System.out.println(number);
-//        User user = userService.getUser((Integer) number);
-//        return UserResponse.builder()
-//                .uuid(user.getUserUuid())
-//                .name(user.getUserName())
-//                .number(user.getUserNumber())
-//                .coin(user.getCoin())
-//                .build();
-        return null;
+        User user = userService.getUser(Integer.valueOf((String) number));
+        return UserResponse.builder()
+                .uuid(user.getUserUuid())
+                .name(user.getUserName())
+                .number(user.getUserNumber())
+                .coin(user.getCoin())
+                .build();
+    }
+
+    @JWTRequired
+    @GetMapping("/booth")
+    @ResponseStatus(HttpStatus.OK)
+    public GetUserResponse getUserByUuid(@AuthenticationPrincipal Object boothId, @RequestParam("uuid") String uuid) {
+        User user = userService.getUserByUuid(uuid);
+        return GetUserResponse.builder()
+                .uuid(user.getUserUuid())
+                .name(user.getUserName())
+                .number(user.getUserNumber())
+                .coin(user.getCoin())
+                .build();
     }
 }
