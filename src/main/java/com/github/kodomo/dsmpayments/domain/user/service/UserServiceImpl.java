fea60e40ab0de5dcfa329929e0544ca55e985ab1
minki son <migsking@naver.com>
2021-06-30 11:10:07 +0900

    [CREATE] Teacher Service

--- src/main/java/com/github/kodomo/dsmpayments/domain/user/service/UserServiceImpl.java
+++ src/main/java/com/github/kodomo/dsmpayments/domain/user/service/UserServiceImpl.java
@@ -2,9 +2,11 @@ package com.github.kodomo.dsmpayments.domain.user.service;
 
 import com.github.kodomo.dsmpayments.domain.receipt.integrate.ReceiptIntegrate;
 import com.github.kodomo.dsmpayments.domain.user.entity.DMSUser;
+import com.github.kodomo.dsmpayments.domain.user.entity.Teacher;
 import com.github.kodomo.dsmpayments.domain.user.entity.User;
 import com.github.kodomo.dsmpayments.domain.user.exception.LoginFailedException;
 import com.github.kodomo.dsmpayments.domain.user.exception.UserNotFoundException;
+import com.github.kodomo.dsmpayments.domain.user.repository.TeacherRepository;
 import com.github.kodomo.dsmpayments.domain.user.repository.UserRepository;
 import com.github.kodomo.dsmpayments.infra.token.TokenProvider;
 import lombok.RequiredArgsConstructor;
@@ -17,6 +19,7 @@ import java.security.MessageDigest;
 @RequiredArgsConstructor
 public class UserServiceImpl implements UserService {
     private final UserRepository userRepository;
+    private final TeacherRepository teacherRepository;
     private final ReceiptIntegrate receiptIntegrate;
 
     private final TokenProvider tokenProvider;
@@ -29,6 +32,16 @@ public class UserServiceImpl implements UserService {
         if (!user.checkPassword(password)) { throw new LoginFailedException(); }
 
         return tokenProvider.generateAccessToken(String.valueOf(user.getNumber()), "user");
+    }
+
+    @Override
+    public String teacherLogin(String id, String password) {
+        Teacher teacher = teacherRepository.findById(id)
+                .orElseThrow(LoginFailedException::new);
+
+        if (!teacher.checkPassword(password)) { throw new LoginFailedException(); }
+
+        return tokenProvider.generateAccessToken(String.valueOf(teacher.getNumber()), "user");
     }
 
     @Override
